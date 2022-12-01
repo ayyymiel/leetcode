7046f789e462e8242fff1f77f187a775ab40a190
ayyymiel <gozonamiel@gmail.com>
2022-11-30 23:06:16 -0500

    moved stuff around

---
+++ medium/permutations/permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # return list list
+        final_list = []
+
+        # base case
+        if len(nums) == 1:
+            return [nums[:]] # return all elements in the nums list
+
+        # recursive case
+        for i in range(len(nums)):
+            num_case = nums.pop(0) # num_case = 0 
+            
+
+sol = Solution()
+print(permute([1,2,3]))
